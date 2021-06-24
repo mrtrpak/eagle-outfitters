c55c849743092f8268a5ad39e8eef58b30006b54
Michael Trpak <mrtrpak3@gmail.com>
2021-06-24 16:19:20 -0400

    importing images and updates names to match

--- src/components/directory/directory.compnent.jsx
+++ src/components/directory/directory.compnent.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+import footwear from './footwear.jpg';
+import jackets from './jackets.jpg';
+import hats from './hats.jpg';
+import mens from './mens.jpg';
+import womens from './womens.jpg';
+
 class Directory extends Component {
   constructor() {
     super();
