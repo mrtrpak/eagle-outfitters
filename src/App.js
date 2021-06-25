0e286ffbb7103b7d937045ab2b425f74c0dcc42a
Michael Trpak <mrtrpak3@gmail.com>
2021-06-25 14:32:21 -0400

    adding in switch and route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -8,7 +8,9 @@ import HomePage from './pages/homepage.component';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
