6648b329cbaf10360c4e2c797131878eb03b552d
Michael Trpak <mrtrpak3@gmail.com>
2021-06-28 15:39:50 -0400

    exporting component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,13 @@ const CollectionPreview = ({ title }) => {
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-
+      {
+        items.map(item => {
+          return <div key={item.id}>{item.name}</div>
+        })
+      }
     </div>
   </div>
-}
+};
+
+export default CollectionPreview;
