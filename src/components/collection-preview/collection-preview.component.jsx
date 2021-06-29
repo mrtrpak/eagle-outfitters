a8ad243a38f9edfa95dce345b77d5d795c70d5fe
Michael Trpak <mrtrpak3@gmail.com>
2021-06-29 15:54:07 -0400

    fixing error

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
@@ -13,6 +13,6 @@ const CollectionPreview = ({ title, items }) => {
       }
     </div>
   </div>
-};
+);
 
 export default CollectionPreview;
