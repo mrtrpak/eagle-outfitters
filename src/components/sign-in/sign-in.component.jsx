abefffbb29c76a8323bc0908c51b065f737580fc
Michael Trpak <mrtrpak3@gmail.com>
2021-06-30 15:25:06 -0400

    creating sign in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './sign-in.styles.scss';
+
+class SignIn extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+
+      </div>
+    );
+  };
+};
+
+export default SignIn;
