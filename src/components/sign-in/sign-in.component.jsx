6180c10eaed27a7671f031bf5dab205be68d2861
Michael Trpak <mrtrpak3@gmail.com>
2021-06-30 15:40:57 -0400

    adding into sign in component with base functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -12,10 +12,46 @@ class SignIn extends Component {
     };
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { email, password } = this.state;
+    const { handleChange, handleSubmit } = this;
     return (
       <div className="sign-in">
+        <h2>I Have an Account</h2>
+        <span>Sign in with your email and password</span>
 
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="email" 
+            name="email" 
+            value={email}
+            onChange={handleChange} 
+            required 
+          />
+          <label>Email</label>
+          <input 
+            type="password" 
+            name="password" 
+            value={password}
+            onChange={handleChange} 
+            required 
+          />
+          <label>Password</label>
+
+          <input type="submit" value="submit form" />
+        </form>
       </div>
     );
   };
