555a0ceec8e23059b04b767a8c9cd48bbf5e97f1
Michael Trpak <mrtrpak3@gmail.com>
2021-06-30 15:44:38 -0400

    importing in sign in component

--- src/pages/signIn-signUp/signIn-signUp.component.jsx
+++ src/pages/signIn-signUp/signIn-signUp.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './signIn-signUp.styles.scss';
 
+import SignIn from '../../components/sign-in/sign-in.component.jsx';
+
 const signInAndSignUpPage = () => (
   <div className="signIn-signUp">
-
+    <SignIn />
   </div>
 );
 
