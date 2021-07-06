de88ba128f94de719e28ab141f370534adc5c869
Michael Trpak <mrtrpak3@gmail.com>
2021-07-06 17:17:44 -0400

    updates

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -55,7 +55,7 @@ class SignIn extends Component {
           />
 
           <CustomButton type="submit">
-            sign in
+            sign in           
           </CustomButton>
         </form>
       </div>
