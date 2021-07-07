8adad736edef995258f680a29a9a98f713ffa412
Michael Trpak <mrtrpak3@gmail.com>
2021-07-07 16:39:09 -0400

    fixing error

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,7 @@ export const createUserProfileDocument =
   async (userAuth, additionalData) => {
     if (!userAuth) return;
 
-    const userRef = firestore.doc("users/") = firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
     const snapShot = await userRef.get();
 
