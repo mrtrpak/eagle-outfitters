77b7db8e42379736bb3978524f2f6480a8b0b3b6
Michael Trpak <mrtrpak3@gmail.com>
2021-07-07 14:22:39 -0400

    adding in firebase folder with utils file

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyC1gzURDSC6hAOgjChNfuYAslJkUdyVaXs",
+  authDomain: "eagle-outfitters.firebaseapp.com",
+  projectId: "eagle-outfitters",
+  storageBucket: "eagle-outfitters.appspot.com",
+  messagingSenderId: "350916008969",
+  appId: "1:350916008969:web:59d33eb35904c43df5ecd8",
+  measurementId: "G-X8627W5W5Q"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
