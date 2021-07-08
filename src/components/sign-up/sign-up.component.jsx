ff2cbc06d8007da3933799551604090d1b4ef846
Michael Trpak <mrtrpak3@gmail.com>
2021-07-08 16:54:16 -0400

    creating sign up component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils.js';
+
+import './sign-up.styles.scss';
+
+import FormInput from '../form-input/form-input.component.jsx';
+import CustomButton from '../custom-button/custom-button.component.jsx';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  };
+};
+
+export default SignUp;
