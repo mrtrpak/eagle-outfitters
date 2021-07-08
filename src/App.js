b948190fb6c970da28c783ae5da465c9c367b15d
Michael Trpak <mrtrpak3@gmail.com>
2021-07-08 16:20:04 -0400

    setting up snapShot and fixing errors

--- src/App.js
+++ src/App.js
@@ -22,10 +22,23 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   };
 
@@ -36,7 +49,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUserd} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,8 @@ const config = {
   measurementId: "G-X8627W5W5Q"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = 
   async (userAuth, additionalData) => {
     if (!userAuth) return;
@@ -35,9 +37,8 @@ export const createUserProfileDocument =
         console.log("error creating user", error.message);
       };
     };
+    return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
