c06c214e706348d3fe0adb67c659ca3d5acf84d2
Michael Trpak <mrtrpak3@gmail.com>
2021-07-08 17:12:55 -0400

    adding in form inputs

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -18,10 +18,56 @@ class SignUp extends Component {
       confirmPassword: ""
     };
   };
-  
-  render() {
+
+  render({ displayName, email, password, confirmPassword }) {
     return (
-      <div></div>
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          >
+
+          </FormInput>
+          <FormInput
+            type="text"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          >
+
+          </FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          >
+
+          </FormInput>
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          >
+
+          </FormInput>
+        </form>
+
+      </div>
     );
   };
 };
