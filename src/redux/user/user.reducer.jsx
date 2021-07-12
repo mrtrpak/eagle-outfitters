d09eb8514d6428d56df66239a507c5778330900b
Michael Trpak <mrtrpak3@gmail.com>
2021-07-12 13:58:48 -0400

    adding to user reducer

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,5 +1,17 @@
-const userReducer = (state, action) => {
-  
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload
+      }
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
