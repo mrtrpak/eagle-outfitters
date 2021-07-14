4bb76096dfbd66df5e3ed8451179e6d67e6c790b
Michael Trpak <mrtrpak3@gmail.com>
2021-07-14 15:01:57 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 
 import App from './App';
-import store from './redux/store.js';
+import { store } from './redux/store.js';
 
 ReactDOM.render(
   <Provider store={store}>
