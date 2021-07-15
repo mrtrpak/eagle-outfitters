4cc1b36d9e3ed321bbc19c9cee08027201496189
Michael Trpak <mrtrpak3@gmail.com>
2021-07-15 16:32:27 -0400

    updating components using redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -6,20 +6,24 @@ import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.component.jsx';
 import { addItem } from '../../redux/cart/cart.actions.js';
 
-const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
-  <div className="collection-item">
-    <div className="image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">${price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
+    <div className="collection-item">
+      <div className="image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </CustomButton>
     </div>
-    <CustomButton inverted>
-      ADD TO CART
-    </CustomButton>
-  </div>
-);
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
