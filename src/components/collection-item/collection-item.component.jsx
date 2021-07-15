f20a6d27afccd215351c32e7e2244c86e433e199
Michael Trpak <mrtrpak3@gmail.com>
2021-07-15 15:38:42 -0400

    adding to redux in cart to update item count

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './collection-item.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component.jsx';
+import { addItem } from '../../redux/cart/cart.actions.js';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
   <div className="collection-item">
     <div className="image"
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
     <div className="collection-footer">
       <span className="name">{name}</span>
-      <span className="price">{price}</span>
+      <span className="price">${price}</span>
     </div>
     <CustomButton inverted>
       ADD TO CART
@@ -19,4 +21,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
   </div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+  null, 
+  mapDispatchToProps
+)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from './cart.types.js';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 export const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,11 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
+      }
     default:
       return state;
   };
