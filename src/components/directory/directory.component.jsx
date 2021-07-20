5c6448a264303db4834b894426fbd2a260515f1c
Michael Trpak <mrtrpak3@gmail.com>
2021-07-20 14:34:20 -0400

    creating directory redux folder

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,51 +4,12 @@ import './directory.styles.scss';
 
 import MenuItem from '../menu-item/menu-item.component';
 
-import footwear from './photos/footwear.jpg';
-import jackets from './photos/jackets.jpg';
-import hats from './photos/hats.jpg';
-import mens from './photos/mens.jpg';
-import womens from './photos/womens.jpg';
-
 class Directory extends Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: 'footwear',
-          image: footwear,
-          id: 1,
-          linkUrl: ''
-        },
-        {
-          title: 'jackets',
-          image: jackets,
-          id: 2,
-          linkUrl: ''
-        },
-        {
-          title: 'hats',
-          image: hats,
-          id: 3,
-          linkUrl: 'hats'
-        },
-        {
-          title: 'mens',
-          image: mens,
-          size: 'large',
-          id: 4,
-          linkUrl: ''
-        },
-        {
-          title: 'womens',
-          image: womens,
-          size: 'large',
-          id: 5,
-          linkUrl: ''
-        }
-      ]
+      
     }
   };
 
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,40 @@
+import footwear from './photos/footwear.jpg';
+import jackets from './photos/jackets.jpg';
+import hats from './photos/hats.jpg';
+import mens from './photos/mens.jpg';
+import womens from './photos/womens.jpg';
+
+export const sections = [
+  {
+    title: 'footwear',
+    image: footwear,
+    id: 1,
+    linkUrl: ''
+  },
+  {
+    title: 'jackets',
+    image: jackets,
+    id: 2,
+    linkUrl: ''
+  },
+  {
+    title: 'hats',
+    image: hats,
+    id: 3,
+    linkUrl: 'hats'
+  },
+  {
+    title: 'mens',
+    image: mens,
+    size: 'large',
+    id: 4,
+    linkUrl: ''
+  },
+  {
+    title: 'womens',
+    image: womens,
+    size: 'large',
+    id: 5,
+    linkUrl: ''
+  }
+];
