7210332f8b88d2568eae0908bc4bedaafcdd0253
Michael Trpak <mrtrpak3@gmail.com>
2021-07-20 14:42:29 -0400

    adding directory reducer to root and adding in initial state for directory redux

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,37 +4,46 @@ import hats from './photos/hats.jpg';
 import mens from './photos/mens.jpg';
 import womens from './photos/womens.jpg';
 
-export const sections = [
-  {
-    title: 'footwear',
-    image: footwear,
-    id: 1,
-    linkUrl: ''
-  },
-  {
-    title: 'jackets',
-    image: jackets,
-    id: 2,
-    linkUrl: ''
-  },
-  {
-    title: 'hats',
-    image: hats,
-    id: 3,
-    linkUrl: 'hats'
-  },
-  {
-    title: 'mens',
-    image: mens,
-    size: 'large',
-    id: 4,
-    linkUrl: ''
-  },
-  {
-    title: 'womens',
-    image: womens,
-    size: 'large',
-    id: 5,
-    linkUrl: ''
-  }
-];
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: 'footwear',
+      image: footwear,
+      id: 1,
+      linkUrl: ''
+    },
+    {
+      title: 'jackets',
+      image: jackets,
+      id: 2,
+      linkUrl: ''
+    },
+    {
+      title: 'hats',
+      image: hats,
+      id: 3,
+      linkUrl: 'hats'
+    },
+    {
+      title: 'mens',
+      image: mens,
+      size: 'large',
+      id: 4,
+      linkUrl: ''
+    },
+    {
+      title: 'womens',
+      image: womens,
+      size: 'large',
+      id: 5,
+      linkUrl: ''
+    }
+  ]
+};
+
+export const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  };
+};
