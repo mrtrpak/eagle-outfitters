877ff421eb86afef51e4a14cc3bdcc91b36cbf8d
Michael Trpak <mrtrpak3@gmail.com>
2021-07-20 17:02:39 -0400

    adding redux imports and map state to props function

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './category.styes.scss';
 
+import { selectCollection } from '../../redux/shop/shop.selectors.js';
 import CollectionItem from '../../components/collection-item/collection-item.component.jsx';
 
-const CollectionPage = ({ match }) => (
-  <div className="collection">
+const CollectionPage = ({ collection }) => (
+  <div className="collection-page">
     <h2>COLLECTION PAGE</h2>
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
