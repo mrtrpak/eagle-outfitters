67ec8179729d4f49d374356ab92d6d2732ac0732
Michael Trpak <mrtrpak3@gmail.com>
2021-07-20 16:46:47 -0400

    updating naming for better consistency

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,10 +4,10 @@ import './category.styes.scss';
 
 import CollectionItem from '../../components/collection-item/collection-item.component.jsx';
 
-const CategoryPage = () => (
-  <div className="category">
-    <h2>CATEGORY PAGE</h2>
+const CollectionPage = ({ match }) => (
+  <div className="collection">
+    <h2>COLLECTION PAGE</h2>
   </div>
 );
 
-export default CategoryPage;
+export default CollectionPage;
