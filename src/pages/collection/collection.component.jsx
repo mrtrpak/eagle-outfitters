babd73b0ab38835eeead035f59cb3d69c620a0f8
Michael Trpak <mrtrpak3@gmail.com>
2021-07-22 16:07:16 -0400

    dynamtically updating the collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,11 +6,20 @@ import './collection.styles.scss';
 import { selectCollection } from '../../redux/shop/shop.selectors.js';
 import CollectionItem from '../../components/collection-item/collection-item.component.jsx';
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection-page">
-    <h2>COLLECTION PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
+  return (
+    <div className="collection-page">
+      <h2>{title}</h2>
+      <div className="items">
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
